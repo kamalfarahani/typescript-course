5b74fdc9c6eaf451cca6a2c976f60dd14a102d67
Kamal (Riwogo) <pmer93@gmail.com>
2019-01-30 14:27:13 +0330

    add namespace examples

---
+++ app.ts
@@ -0,0 +1,7 @@
+/// <reference path="./circleMath.ts"/>
+/// <reference path="./rectangleMath.ts"/>
+
+import CircleMath = MyMath.Circle;
+
+console.log(MyMath.calculateRectangle(10, 2));
+console.log(CircleMath.calculateCircumference(10));
